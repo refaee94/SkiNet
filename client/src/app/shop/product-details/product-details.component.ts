0e9d15217e8d998435316a34141ef66520413c8c
mohammed <m.refaee123580@gmail.com>
2020-08-06 22:36:27 +0400

    end routing

---
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/shared/models/product';
+import { ShopService } from '../shop.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.scss']
+})
+export class ProductDetailsComponent implements OnInit {
+product:IProduct
+  constructor(private shopService:ShopService,private activatetroute:ActivatedRoute) { }
+
+  ngOnInit( ) {
+    this.loadProduct()
+  }
+loadProduct(){
+this.shopService.getProduct(+this.activatetroute.snapshot.paramMap.get('id')).subscribe(product=>{this.product=product},error=>{
+  console.log(error)
+})
+
+}
+}
---
+++ client/src/app/shop/shop-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Route } from '@angular/compiler/src/core';
+import { ShopComponent } from './shop.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes:Routes=[
+
+
+  {path:'',component:ShopComponent},
+{path:':id',component:ProductDetailsComponent},
+]
+@NgModule({
+  declarations: [],
+  imports: [
+RouterModule.forChild(routes)  ]
+,exports:[RouterModule]
+})
+export class ShopRoutingModule { }
