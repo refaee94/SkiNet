030600cbc66721f370b20fa9a47eed85077ae19b
mohammed <m.refaee123580@gmail.com>
2020-08-13 23:42:33 +0400

    orders

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -13,7 +13,11 @@ namespace API.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {services.AddScoped<ITokenService,TokenService>();
             services.AddScoped<IProductRepository, ProductRepository>();
+                        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
             services.AddScoped<IBasketRepository,BasketRepository>();
+                        services.AddScoped<IOrderService,OrderService>();
+
             services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
                         services.Configure<ApiBehaviorOptions>(options =>
             {
